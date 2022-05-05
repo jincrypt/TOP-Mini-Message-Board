0a0bc879ffffc1061d8ac026f239e4e507913ade
jincrypt <leung.mka@gmail.com>
2022-05-05 19:25:52 -0400

    Completed project

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Messageboard', messages });
 });
+
+
+/* POST message */
+router.post('/new', function(req,res,next) {
+  const newMessage = {
+    text: req.body.message,
+    user: req.body.user,
+    added: new Date()
+  }
+  messages.unshift(newMessage);
+  res.redirect('/');
+})
 
 module.exports = router;
